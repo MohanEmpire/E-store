349f02388ddc68f33e97e17bdf820eb34d5cc8e6
MohanEmpire <130851990+MohanEmpire@users.noreply.github.com>
2023-04-20 11:52:30 +0530

    second commit
    pom file for redirectfunctionality

---
+++ RedirectFunctionalityPOM.java
@@ -0,0 +1,10 @@
+package OnlineStorePOM;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class RedirectFunctionalityPOM {
+	@FindBy(linkText = "Online Catalog")
+	public static WebElement onlinecatlog;
+
+}
